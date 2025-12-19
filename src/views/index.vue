3cd6f5c964c319ee33ed92232a99bc16ad052ee3
weiyiming <997990179@email.com>
2025-12-19 07:00:17 +0700

    fix:配置文件增加vue-codemirror和@codemirror/lang-sql的依赖

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,7 @@
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
         <el-row>
           <el-col :span="12">
-            <h2>项目介绍</h2>
+            <h2>此处省略一万字</h2>
           </el-col>
         </el-row>
       </el-col>
@@ -12,33 +12,7 @@
       <el-col :sm="24" :lg="12" style="padding-left: 50px">
         <el-row>
           <el-col :span="12">
-            <h2>技术选型</h2>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="6">
-            <h4>后端技术</h4>
-            <ul>
-              <li>SpringBoot</li>
-              <li>Spring Security</li>
-              <li>JWT</li>
-              <li>MyBatis</li>
-              <li>Druid</li>
-              <li>Fastjson</li>
-              <li>...</li>
-            </ul>
-          </el-col>
-          <el-col :span="6">
-            <h4>前端技术</h4>
-            <ul>
-              <li>Vue</li>
-              <li>Vuex</li>
-              <li>Element-ui</li>
-              <li>Axios</li>
-              <li>Sass</li>
-              <li>Quill</li>
-              <li>...</li>
-            </ul>
+            <h2>此处省略一万字</h2>
           </el-col>
         </el-row>
       </el-col>
